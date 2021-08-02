e6c130df77d92e8fd0f528a01ac02af8e81f4827
charangopisetty <gopisettycharan@gmail.com>
2021-08-02 11:00:51 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import LandingSection from './Components/LandingSection/LandingSection';
+import './App.css';
+
+const App = () => {
+  return <main><LandingSection/>
+  </main>
+  
+};
+
+export default App;
---
+++ src/Components/LandingSection/LandingSection.jsx
@@ -0,0 +1,11 @@
+import './LandingSection.css'
+
+const LandingSection = () => {
+    return (
+        <div>
+            Landing Section
+        </div>
+    )
+}
+
+export default LandingSection
